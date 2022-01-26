e71693a154489c520cb8a94e39d4398e127de663
Ken Schenke <kenschenke@gmail.com>
2022-01-25 18:54:13 -0600

    Initial commit

---
+++ src/main/java/frc/robot/commands/drivetrain/DriveCommand.java
@@ -0,0 +1,54 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.drivetrain;
+
+import frc.robot.subsystems.DrivetrainSubsystem;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+import java.util.function.DoubleSupplier;
+
+public class DriveCommand extends CommandBase {
+    private final DrivetrainSubsystem drivetrain;
+    private final DoubleSupplier translationXSupplier;
+    private final DoubleSupplier translationYSupplier;
+    private final DoubleSupplier rotationSupplier;
+
+    public DriveCommand(
+            DrivetrainSubsystem drivetrain,
+            DoubleSupplier translationXSupplier,
+            DoubleSupplier translationYSupplier,
+            DoubleSupplier rotationSupplier
+    ) {
+        this.drivetrain = drivetrain;
+        this.translationXSupplier = translationXSupplier;
+        this.translationYSupplier = translationYSupplier;
+        this.rotationSupplier = rotationSupplier;
+
+        addRequirements(drivetrain);
+    }
+
+    @Override
+    public void execute() {
+        double translationXPercent = translationXSupplier.getAsDouble();
+        double translationYPercent = translationYSupplier.getAsDouble();
+        double rotationPercent = rotationSupplier.getAsDouble();
+
+        drivetrain.drive(
+                ChassisSpeeds.fromFieldRelativeSpeeds(
+                        translationXPercent * DrivetrainSubsystem.MAX_VELOCITY_METERS_PER_SECOND,
+                        translationYPercent * DrivetrainSubsystem.MAX_VELOCITY_METERS_PER_SECOND,
+                        rotationPercent * DrivetrainSubsystem.MAX_ANGULAR_VELOCITY_RADIANS_PER_SECOND,
+                        drivetrain.getRotation()
+                )
+        );
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        // Stop the drivetrain
+        drivetrain.drive(new ChassisSpeeds(0.0, 0.0, 0.0));
+    }
+}
